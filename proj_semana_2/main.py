7d922e7b42ff1f6751aaa8630e906c0e2cd1aa0b
giovanaa988 <marquettegiovanaa@gmail.com>
2025-03-20 17:38:56 -0300

    Add files via upload

---
+++ proj_semana_2/main.py
@@ -0,0 +1,136 @@
+#Definindo as variáveis
+opcao_est = 1
+opcao_disc = 2
+opcao_prof = 3
+opcao_turmas = 4
+opcao_matr = 5
+opcao_menu = 6
+
+
+#Construindo o menu principal
+print("***************** MENU PRINCIPAL *****************")
+print("(1) Gerenciar estudantes.")
+print("(2) Gerenciar discplina.")
+print("(3) Gerenciar professores.")
+print("(4) Gerenciar turmas.")
+print("(5) Gerenciar matrículas.")
+print("(6) Sair.")
+
+print(" ")
+
+opcao = int(input("Informe a opção desejada: "))
+print(" ")
+
+#Estudantes
+if opcao == opcao_est:
+    print("----------------- [ESTUDANTES] MENU DE OPERAÇÕES -----------------")
+    print(" ")
+    print("(1) Incluir.")
+    print("(2) Listar.")
+    print("(3) Atualizar.")
+    print("(4) Excluir.")
+    print("(5) Voltar ao menu principal.")
+
+    print(" ")
+
+    opcao2 = int(input("Informe a ação desejada: "))
+    print(" ")
+
+    if opcao2 == 1 or 2 or 3 or 4 or 5 or 6:
+        print("######################## ATUALIZAÇÃO ########################")
+        print(" ")
+        print("Finalizando aplicação...")
+
+        print(" ")
+
+#Professores
+if opcao == opcao_disc:
+    print("----------------- [DISCIPLINAS] MENU DE OPERAÇÕES -----------------")
+    print(" ")
+    print("(1) Incluir.")
+    print("(2) Listar.")
+    print("(3) Atualizar.")
+    print("(4) Excluir.")
+    print("(5) Voltar ao menu principal.")
+
+    print(" ")
+
+    opcao2 = int(input("Informe a ação desejada: "))
+    print(" ")
+
+    if opcao2 == 1 or 2 or 3 or 4 or 5:
+        print("######################## ATUALIZAÇÃO ########################")
+        print(" ")
+        print("Finalizando aplicação...")
+
+        print(" ")
+
+#Professores
+if opcao == opcao_prof:
+    print("----------------- [PROFESSORES] MENU DE OPERAÇÕES -----------------")
+    print(" ")
+    print("(1) Incluir.")
+    print("(2) Listar.")
+    print("(3) Atualizar.")
+    print("(4) Excluir.")
+    print("(5) Voltar ao menu principal.")
+
+    print(" ")
+
+    opcao2 = int(input("Informe a ação desejada: "))
+    print(" ")
+
+    if opcao2 == 1 or 2 or 3 or 4 or 5:
+        print("######################## ATUALIZAÇÃO ########################")
+        print(" ")
+        print("Finalizando aplicação...")
+
+        print(" ")
+
+#Turmas
+if opcao == opcao_turmas:
+    print("----------------- [TURMAS] MENU DE OPERAÇÕES -----------------")
+    print(" ")
+    print("(1) Incluir.")
+    print("(2) Listar.")
+    print("(3) Atualizar.")
+    print("(4) Excluir.")
+    print("(5) Voltar ao menu principal.")
+
+    print(" ")
+
+    opcao2 = int(input("Informe a ação desejada: "))
+    print(" ")
+
+    if opcao2 == 1 or 2 or 3 or 4 or 5:
+        print("######################## ATUALIZAÇÃO ########################")
+        print(" ")
+        print("Finalizando aplicação...")
+        print(" ")
+
+#Matrículas
+if opcao == opcao_matr:
+    print("----------------- [MATRÍCULAS] MENU DE OPERAÇÕES -----------------")
+    print(" ")
+    print("(1) Incluir.")
+    print("(2) Listar.")
+    print("(3) Atualizar.")
+    print("(4) Excluir.")
+    print("(5) Voltar ao menu principal.")
+    print(" ")
+
+    opcao2 = int(input("Informe a ação desejada: "))
+    print(" ")
+
+    if opcao2 == 1 or 2 or 3 or 4 or 5:
+        print("######################## ATUALIZAÇÃO ########################")
+        print(" ")
+        print("Finalizando aplicação...")
+        print(" ")
+
+#Menu principal
+if opcao == opcao_menu:
+    print("######################## ATUALIZAÇÃO ########################")
+    print(" ")
+    print("Finalizando aplicação...")
+    print(" ")
